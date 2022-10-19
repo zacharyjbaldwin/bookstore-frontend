7d4af21aa91e33a67ba973a46089fc70fbe87db2
Zachary Baldwin <zacharyjbaldwin@gmail.com>
2022-10-18 21:49:32 -0500

    working on checkout flow

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { ToastrService } from 'ngx-toastr';
 import { AddAddressModalComponent } from '../modals/add-address-modal/add-address-modal.component';
 import { Address } from '../models/address.model';
+import { AuthService } from '../services/auth.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-checkout',
@@ -11,15 +14,21 @@ import { Address } from '../models/address.model';
 })
 export class CheckoutComponent implements OnInit {
 
+  public addresses: Address[] = [];
   public currentPage: String = 'shipping';
   public deliveryStandard: Date;
   public selectedShippingSpeed: boolean = false;
   public selectedAddress: boolean = false;
   public paymentForm: FormGroup;
   public addAddressModal?: BsModalRef;
-  // public addresses: {_id: string, address: Address}[] = [];
+  public showPlaceOrderButton: boolean = true;
 
-  constructor(private modalService: BsModalService) {
+  constructor(
+    private authService: AuthService,
+    private modalService: BsModalService,
+    private userService: UserService,
+    private toastr: ToastrService
+  ) {
     this.deliveryStandard = new Date();
     this.deliveryStandard.setDate(this.deliveryStandard.getDate() + 3);
     this.paymentForm = new FormGroup({
@@ -31,6 +40,8 @@ export class CheckoutComponent implements OnInit {
       expireMonth: new FormControl('january', [Validators.required]),
       expireYear: new FormControl(2022, [Validators.required, Validators.min(1000), Validators.max(9999)])
     });
+
+    this.fetchAddresses();
   }
 
   ngOnInit(): void {
@@ -48,9 +59,42 @@ export class CheckoutComponent implements OnInit {
     this.currentPage = 'review';
   }
 
+  // openAddAddressModal() {
+  //   this.addAddressModal = this.modalService.show(AddAddressModalComponent, { class: 'modal-md' });
+  //   (this.addAddressModal.content as AddAddressModalComponent).affirm.subscribe((address: Address) => {
+  //   });
+  // }
+
   openAddAddressModal() {
     this.addAddressModal = this.modalService.show(AddAddressModalComponent, { class: 'modal-md' });
-    (this.addAddressModal.content as AddAddressModalComponent).affirm.subscribe((address: Address) => {
+    (this.addAddressModal.content as AddAddressModalComponent).affirm.subscribe((res: Address) => {
+      const address: Address = {
+        sendTo: res.sendTo,
+        addrLine1: res.addrLine1,
+        addrLine2: (res.addrLine2 ? res.addrLine2 : undefined),
+        city: res.city,
+        state: res.state,
+        zip: res.zip
+      };
+
+      this.addAddress(address);
+    });
+  }
+
+  addAddress(address: Address) {
+    this.userService.addAddress(address).subscribe((res) => {
+      this.toastr.success('Added address.');
+      this.fetchAddresses();
+    });
+  }
+
+  private fetchAddresses() {
+    // this.loading = true;
+
+    this.userService.getAddressesById(this.authService.getUserId()).subscribe((addresses: Address[]) => {
+      this.addresses = addresses;
+      // this.count = this.addresses.length;
+      // this.loading = false;
     });
   }
 }
