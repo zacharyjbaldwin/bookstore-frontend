487bf21a7cd54f34f9bceaa78cf0e5b11bcf9b85
Zachary Baldwin <zacharyjbaldwin@gmail.com>
2022-11-11 00:03:42 -0600

    update production api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://cs4386-bookstore-api.herokuapp.com',
+  apiUrl: 'https://api.btl-books.com',
   stringFor: {
     true: 'FDBC5F518E44154B29B54B77E7ACE79DAB5D55873552BF23B9B2F2EE0587B229',
     false: 'C4356E4846A6CCC89006B1216C4D0A9F3ED59B3395E825C3003F0576C5FA3A61',
